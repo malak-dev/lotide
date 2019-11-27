a03ebe0148598c74dc2171debc219e678583ce31
malak <malak-y@live.com>
2019-11-27 19:32:26 +0000

    countLetters in Lotide

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed:" + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed:" + actual + " !== " + expected);
+  }
+};
+const countLetters = function (str) {
+  var str2 = str.split(' ').join('');
+  const result = {};
+
+  for (let count of str2) {
+    if (result[count]) {
+      result[count] += 1;
+    } else {
+      result[count] = 1;
+
+    }
+  }
+  return result;
+
+};
+
+
+console.log(countLetters("lighthouse in the house"));
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
