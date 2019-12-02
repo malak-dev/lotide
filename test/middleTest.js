b5b802ea188bf8e21339a34519b8021f77015025
malak <malak-y@live.com>
2019-12-02 19:36:43 +0000

    Refactor middleTest and tailTest wirh M & C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,9 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle')
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [1, 2, 6]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [4, 5]);
+const assert = require('chai').assert;
+const middle = require('../middle');
+describe("#middle", () => {
+  it('returns [3] for [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+});
+
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,3 +1,9 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertArraysEqual = require('../assertArraysEqual');
-assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+
+describe("#tail", () => {
+  it('returns "Lighthouse", "Labs" for "Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
+
